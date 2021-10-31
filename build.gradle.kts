68c43075f3c309ebeaba657b24640d69268290f5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-10-31 21:39:38 +0000

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+	id("com.android.application") version "4.2.2" apply false
+	id("net.twisterrob.root") version "0.12.publish-SNAPSHOT"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+rootProject.name = "Sun"
+
+include(":app")
+include(":lib")
+
+pluginManagement {
+	repositories {
+		google()
+		mavenCentral()
+		maven { name = "Sonatype SNAPSHOTs"; setUrl("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+	}
+	resolutionStrategy {
+		eachPlugin {
+			when (requested.id.id) {
+				"com.android.application" -> {
+					useModule("com.android.tools.build:gradle:${requested.version}")
+				}
+				"net.twisterrob.root",
+				"net.twisterrob.java",
+				"net.twisterrob.android-lib",
+				"net.twisterrob.android-app" -> {
+	   				// useModule("net.twisterrob.gradle:twister-convention-plugins:${requested.version}")
+					useModule("net.twisterrob.gradle:plugin:${requested.version}")
+				}
+			}
+		}
+	}
+}
