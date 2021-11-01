f98a69b4017e891d75da1be4e9ba110adf5b7316
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-01 18:52:30 +0000

    Centralize versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-	id("com.android.application") version "4.2.2" apply false
-	id("net.twisterrob.root") version "0.12"
+	id("com.android.application") apply false
+	id("net.twisterrob.root")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,15 +11,16 @@ pluginManagement {
 	}
 	resolutionStrategy {
 		eachPlugin {
+			// REPORT requested.version is null when using plugins {} block just above on Gradle 6.9.1.
 			when (requested.id.id) {
 				"com.android.application" -> {
-					useModule("com.android.tools.build:gradle:${requested.version}")
+					useModule("com.android.tools.build:gradle:4.2.2")
 				}
 				"net.twisterrob.root",
 				"net.twisterrob.java",
 				"net.twisterrob.android-lib",
 				"net.twisterrob.android-app" -> {
-	   				useModule("net.twisterrob.gradle:twister-convention-plugins:${requested.version}")
+	   				useModule("net.twisterrob.gradle:twister-convention-plugins:0.12")
 				}
 			}
 		}
