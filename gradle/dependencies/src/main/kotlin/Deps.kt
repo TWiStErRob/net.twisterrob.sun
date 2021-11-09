0a3af93265a85f13548a9d96ba18cd512f670dd2
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-09 09:31:07 +0000

    Update compileSdkVersion to 31

--- gradle/dependencies/src/main/kotlin/Deps.kt
+++ gradle/dependencies/src/main/kotlin/Deps.kt
@@ -1,11 +1,12 @@
 object Deps {
 
 	object Android {
-		const val compileSdkVersion = 29
 
 		/**
 		 * When changing this, update CI too (platforms;android-xx).
 		 */
+		const val compileSdkVersion = 31
+
 		const val minSdkVersion = 14
 
 		/**
@@ -34,6 +35,7 @@ object Deps {
 	}
 
 	object Test {
+
 		const val paparazzi = "app.cash.paparazzi:paparazzi:0.8.0"
 		const val junit4 = "junit:junit:4.13.2"
 		const val mockito = "org.mockito:mockito-core:4.0.0"
