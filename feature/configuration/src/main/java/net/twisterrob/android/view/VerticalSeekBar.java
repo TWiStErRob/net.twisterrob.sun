91f7a780421ba6a72d4cc68bea9a255d8ed73faf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-11-09 10:02:34 +0000

    Fix unnecessary margins and appcompat

--- feature/configuration/src/main/java/net/twisterrob/android/view/VerticalSeekBar.java
+++ feature/configuration/src/main/java/net/twisterrob/android/view/VerticalSeekBar.java
@@ -5,11 +5,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatSeekBar;
 
-public class VerticalSeekBar extends SeekBar {
+public class VerticalSeekBar extends AppCompatSeekBar {
 	public VerticalSeekBar(Context context) {
 		super(context);
 	}
