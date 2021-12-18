6e644b9d7d9a3d118c9d7cba52a45193adddd737
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-12-18 02:11:45 +0000

    Remove ObsoleteLintCustomCheck exclusion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-	// TODEL ObsoleteLintCustomCheck once on 7.x
 	id("com.android.application") version "7.0.4" apply false
 	id("app.cash.paparazzi") version "0.8.0" apply false
 	id("net.twisterrob.root") version "0.13-SNAPSHOT"
