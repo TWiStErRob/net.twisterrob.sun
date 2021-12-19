f5cb0ba57968dc37fe95e57c7ce9bbe3577e73a1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-12-19 13:13:54 +0000

    Make CI fail on lint violations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	id("com.android.application") version "7.0.4" apply false
 	id("app.cash.paparazzi") version "0.8.0" apply false
-	id("net.twisterrob.root") version "0.13-SNAPSHOT"
-	id("net.twisterrob.quality") version "0.13-SNAPSHOT"
+	id("net.twisterrob.root") version "0.13.fix195-SNAPSHOT"
+	id("net.twisterrob.quality") version "0.13.fix195-SNAPSHOT"
 	id("project-dependencies") apply false
 }
