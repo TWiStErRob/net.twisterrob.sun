69e3561861ba998884da60ddb0b8701af707bdec
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-12-20 17:42:34 +0000

    Work around cashapp/paparazzi#343

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	id("com.android.application") version "7.0.4" apply false
+	// TODEL workaround in settings.gradle.kts once released.
 	id("app.cash.paparazzi") version "0.9.0" apply false
 	id("net.twisterrob.root") version "0.13"
 	id("net.twisterrob.quality") version "0.13"
