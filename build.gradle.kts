45f1ae55ed47a76cbe2fbbe067ec747e0874ec3c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-12-20 17:42:07 +0000

    Upgrade to 0.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("com.android.application") version "7.0.4" apply false
-	id("app.cash.paparazzi") version "0.8.0" apply false
+	id("app.cash.paparazzi") version "0.9.0" apply false
 	id("net.twisterrob.root") version "0.13"
 	id("net.twisterrob.quality") version "0.13"
 	id("project-dependencies") apply false
--- gradle/dependencies/src/main/kotlin/Deps.kt
+++ gradle/dependencies/src/main/kotlin/Deps.kt
@@ -39,7 +39,7 @@ object Deps {
 
 	object Test {
 
-		const val paparazzi = "app.cash.paparazzi:paparazzi:0.8.0"
+		const val paparazzi = "app.cash.paparazzi:paparazzi:0.9.0"
 		const val junit4 = "junit:junit:4.13.2"
 		const val mockito = "org.mockito:mockito-core:4.0.0"
 		const val paramInjector = "com.google.testparameterinjector:test-parameter-injector:1.5"
