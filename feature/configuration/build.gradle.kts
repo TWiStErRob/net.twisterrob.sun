a0bec956d592e9eba14a897ca7418cbc519d16f6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-01-28 09:21:35 +0000

    Fix reflection warning

--- feature/configuration/build.gradle.kts
+++ feature/configuration/build.gradle.kts
@@ -24,3 +24,17 @@ android {
 		buildConfig = true
 	}
 }
+
+tasks.withType<Test>().configureEach {
+	jvmArgs(
+		// Hide the huge yellow WARNING:
+		// > An illegal reflective access operation has occurred
+		// > Illegal reflective access by LocationPermissionCompatTest
+		// (.../feature/configuration/build/intermediates/javac/debugUnitTest/classes/)
+		// to field java.lang.reflect.Field.modifiers
+		// > Please consider reporting this to the maintainers of LocationPermissionCompatTest
+		// > Use --illegal-access=warn to enable warnings of further illegal reflective access operations
+		// > All illegal access operations will be denied in a future release
+		"--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED",
+	)
+}
