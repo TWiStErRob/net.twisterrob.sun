277b11de31a6f44e99b1189eb631edae5909efca
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-02-10 22:36:45 +0000

    Konvert SunAngleWidgetProvider

--- component/widget/src/main/java/net/twisterrob/sun/android/SunAngleWidgetProvider.kt
+++ component/widget/src/main/java/net/twisterrob/sun/android/SunAngleWidgetProvider.kt
@@ -1,74 +1,63 @@
-package net.twisterrob.sun.android;
+package net.twisterrob.sun.android
 
-import java.util.Locale;
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.util.Log
+import android.widget.Toast
+import androidx.core.location.LocationListenerCompat
+import net.twisterrob.sun.android.logic.SunAngleWidgetUpdater
+import net.twisterrob.sun.android.logic.WidgetUpdateList
+import java.util.*
+import javax.inject.Inject
 
-import javax.inject.Inject;
+class SunAngleWidgetProvider : LoggingAppWidgetProvider() {
 
-import android.appwidget.AppWidgetManager;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
-import android.util.Log;
-import android.widget.Toast;
+	@Inject lateinit var component: WidgetComponent
+	@Inject lateinit var updater: SunAngleWidgetUpdater
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.location.LocationListenerCompat;
-
-import net.twisterrob.sun.android.logic.SunAngleWidgetUpdater;
-import net.twisterrob.sun.android.logic.WidgetUpdateList;
-
-public class SunAngleWidgetProvider extends LoggingAppWidgetProvider {
-
-	private static final String TAG = "Sun";
-
-	@Inject WidgetComponent component = null;
-	@Inject SunAngleWidgetUpdater updater = null;
-
-	@Override
-	public void onReceive(@NonNull Context context, @NonNull Intent intent) {
-		if (component == null) {
-			DaggerWidgetComponent.factory().create(context).inject(this);
-		}
-		super.onReceive(context, intent);
+	override fun onReceive(context: Context, intent: Intent) {
+		DaggerWidgetComponent.factory().create(context).inject(this)
+		super.onReceive(context, intent)
 	}
 
-	@Override
-	public void onDeleted(@NonNull Context context, @NonNull int[] appWidgetIds) {
-		super.onDeleted(context, appWidgetIds);
-		for (int appWidgetId : appWidgetIds) {
-			SunAngleWidgetPreferences.getPreferences(context, appWidgetId).edit().clear().apply();
+	override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+		super.onDeleted(context, appWidgetIds)
+		for (appWidgetId in appWidgetIds) {
+			SunAngleWidgetPreferences.getPreferences(context, appWidgetId).edit().clear().apply()
 		}
 	}
 
-	@Override
-	public void onUpdate(@NonNull Context context, @NonNull AppWidgetManager appWidgetManager, @NonNull int[] appWidgetIds) {
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		updateAll(context, appWidgetIds);
+	override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds)
+		updateAll(context, *appWidgetIds)
 	}
 
-	private void updateAll(final @NonNull Context context, final @NonNull int... appWidgetIds) {
+	private fun updateAll(context: Context, vararg appWidgetIds: Int) {
 		try {
-			WidgetUpdateList TODOs = new WidgetUpdateList();
-			TODOs.add(appWidgetIds);
-			TODOs.catchup(updater, new LocationListenerCompat() {
-				@Override
-				public void onLocationChanged(@Nullable Location location) {
+			val TODOs = WidgetUpdateList()
+			TODOs.add(*appWidgetIds)
+			TODOs.catchup(updater, object : LocationListenerCompat {
+				override fun onLocationChanged(location: Location) {
 					if (Log.isLoggable(TAG, Log.VERBOSE)) {
-						Log.v(TAG, SunAngleWidgetProvider.this + ".onLocationChanged(" + location + ")");
+						Log.v(TAG, "${this@SunAngleWidgetProvider}.onLocationChanged(${location})")
 					}
-					updater.clearLocation(this);
-					updateAll(context, appWidgetIds);
+					updater.clearLocation(this)
+					updateAll(context, *appWidgetIds)
 				}
-			});
-		} catch (Exception ex) {
-			Log.e(TAG, this + ".updateAll", ex);
-			Toast.makeText(context, ex.toString(), Toast.LENGTH_LONG).show();
+			})
+		} catch (ex: Exception) {
+			Log.e(TAG, "${this}.updateAll", ex)
+			Toast.makeText(context, ex.toString(), Toast.LENGTH_LONG).show()
 		}
 	}
 
-	@Override
-	public @NonNull String toString() {
-		return String.format(Locale.ROOT, "%08x", this.hashCode());
+	override fun toString(): String =
+		"%08x".format(Locale.ROOT, this.hashCode())
+
+	companion object {
+
+		private const val TAG = "Sun"
 	}
 }
