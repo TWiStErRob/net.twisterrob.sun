1a552946158326f1786826b55c91907945830a76
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-02-24 09:12:23 +0000

    Enable project accessors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 rootProject.name = "Sun"
 
+enableFeaturePreviewQuietly("TYPESAFE_PROJECT_ACCESSORS", "Type-safe project accessors")
+
 include(":app")
 include(":feature:configuration")
 include(":feature:preview")
@@ -83,4 +85,23 @@ gradleEnterprise {
 		termsOfServiceUrl = "https://gradle.com/terms-of-service"
 		termsOfServiceAgree = "yes"
 	}
+}
+
+/**
+ * @see <a href="https://github.com/gradle/gradle/issues/19069">Feature request</a>
+ */
+fun Settings.enableFeaturePreviewQuietly(name: String, summary: String) {
+	enableFeaturePreview(name)
+	val logger: Any = org.gradle.util.internal.IncubationLogger::class.java
+		.getDeclaredField("INCUBATING_FEATURE_HANDLER")
+		.apply { isAccessible = true }
+		.get(null)
+
+	@Suppress("UNCHECKED_CAST")
+	val features: MutableSet<String> = org.gradle.internal.featurelifecycle.LoggingIncubatingFeatureHandler::class.java
+		.getDeclaredField("features")
+		.apply { isAccessible = true }
+		.get(logger) as MutableSet<String>
+
+	features.add(summary)
 }
