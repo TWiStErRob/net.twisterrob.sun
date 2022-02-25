b8ba0897588ca726bbf4cc18573a11921e995f85
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-02-25 22:21:21 +0000

    Move Gradle plugins to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
+@Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage") // TODEL Gradle 6.4
 plugins {
-	// TODEL workaround in :app/build.gradle.kts for 194525628 for 7.2+
-	id("com.android.application") version "7.0.4" apply false
-	id("org.jetbrains.kotlin.android") version "1.5.31" apply false
-	// TODEL workaround in settings.gradle.kts once released.
-	id("app.cash.paparazzi") version "0.9.0" apply false
-	id("net.twisterrob.root") version "0.14-20220205.192501-3"
-	id("net.twisterrob.quality") version "0.14-20220205.192501-3"
-	id("io.gitlab.arturbosch.detekt") version "1.19.0" apply false
+	alias(libs.plugins.android.app) apply false
+	alias(libs.plugins.kotlin.android) apply false
+	alias(libs.plugins.paparazzi) apply false
+	alias(libs.plugins.twisterrob.root)
+	alias(libs.plugins.twisterrob.quality)
+	alias(libs.plugins.kotlin.detekt) apply false
 }
 
 buildscript {
