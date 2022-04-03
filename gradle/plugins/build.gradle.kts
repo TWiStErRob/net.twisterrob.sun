23a5ba09d22544add753f207a6284c7dca96aa79
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-03 01:56:22 +0100

    Revert update and ignore it

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("org.gradle.kotlin.kotlin-dsl") version "2.2.0" // see appliedKotlinDslPluginsVersion
+	id("org.gradle.kotlin.kotlin-dsl") version "2.1.7" // see appliedKotlinDslPluginsVersion
 	id("java-gradle-plugin")
 }
 
