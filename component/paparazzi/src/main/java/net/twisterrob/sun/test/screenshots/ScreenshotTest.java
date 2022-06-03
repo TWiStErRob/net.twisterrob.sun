88d7508f1d614aab9343723aacde862d4a2a6529
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-06-03 22:16:37 +0100

    Document

--- component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/ScreenshotTest.java
+++ component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/ScreenshotTest.java
@@ -1,5 +1,8 @@
 package net.twisterrob.sun.test.screenshots;
 
+/**
+ * Marker interface for signaling Paparazzi tests. These won't be executed during normal testing.
+ */
 public interface ScreenshotTest {
 
 }
