e6e2759453c3af0bac2553ecf038ca15293a6b9a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-06-03 21:52:05 +0100

    🙏 @JvmOverloads

--- component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/PaparazziCoat.java
+++ component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/PaparazziCoat.java
@@ -1,7 +1,5 @@
 package net.twisterrob.sun.test.screenshots;
 
-import java.io.File;
-import java.util.Collections;
 import java.util.concurrent.Callable;
 
 import org.junit.rules.RuleChain;
@@ -23,10 +21,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Px;
 import app.cash.paparazzi.DeviceConfig;
 import app.cash.paparazzi.EnvironmentKt;
-import app.cash.paparazzi.HtmlReportWriter;
 import app.cash.paparazzi.Paparazzi;
-import app.cash.paparazzi.RenderExtension;
-import app.cash.paparazzi.SnapshotVerifier;
 
 public class PaparazziCoat implements TestRule {
 
@@ -55,7 +50,7 @@ public class PaparazziCoat implements TestRule {
 	}
 
 	public void snapshot(@NonNull View view) {
-		paparazzi.snapshot(view, null, null, null);
+		paparazzi.snapshot(view);
 	}
 
 	public void snapshotWithSize(@NonNull View view, float width, float height) {
@@ -78,11 +73,7 @@ public class PaparazziCoat implements TestRule {
 				"AppTheme.ScreenshotTest",
 				RenderingMode.NORMAL,
 				true,
-				0.0,
-				Boolean.parseBoolean(System.getProperty("paparazzi.test.verify"))
-						? new SnapshotVerifier(0.0, new File("src/test/snapshots"))
-						: new HtmlReportWriter(),
-				Collections.<RenderExtension>emptySet()
+				0.0
 		);
 	}
 }
