e0f0cabae4ad0644040914c6206f3f5a4ea4676f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-06-03 21:41:24 +0100

    cashapp/paparazzi#308 classpath leak is fixed

--- component/paparazzi/build.gradle.kts
+++ component/paparazzi/build.gradle.kts
@@ -7,8 +7,6 @@ dependencies {
 	api(libs.test.junit4)
 	api(libs.test.mockito)
 	api(libs.test.paramInjector)
-	// TODEL https://github.com/cashapp/paparazzi/pull/308
-	api(platform(libs.kotlin.bom))
 	// TODEL https://github.com/cashapp/paparazzi/issues/306
 	compileOnly(projects.component.awtHack)
 }
