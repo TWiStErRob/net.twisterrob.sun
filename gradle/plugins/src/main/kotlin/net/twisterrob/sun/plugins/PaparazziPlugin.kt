ccd45834907b7a08d3dfc46e9118dcb74ed6068e
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-06-04 17:40:32 +0100

    Suppress warnings

--- gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/PaparazziPlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/PaparazziPlugin.kt
@@ -27,6 +27,20 @@ class PaparazziPlugin : Plugin<Project> {
 					)
 				}
 			}
+			jvmArgs(
+				// Hide the huge yellow WARNING:
+				// > An illegal reflective access operation has occurred
+				// > Illegal reflective access by net.twisterrob.sun.test.screenshots.ClearFinalKt
+				// (file:/P:/projects/workspace/net.twisterrob.sun/component/paparazzi/build/libs/paparazzi.jar)
+				// to field java.lang.reflect.Field.modifiers
+				// > Illegal reflective access by LocationPermissionCompatTest
+				// (.../feature/configuration/build/intermediates/javac/debugUnitTest/classes/)
+				// to field java.lang.reflect.Field.modifiers
+				// > Please consider reporting this to the maintainers of net.twisterrob.sun.test.screenshots.ClearFinalKt
+				// > Use --illegal-access=warn to enable warnings of further illegal reflective access operations
+				// > All illegal access operations will be denied in a future release
+				"--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED",
+			)
 		}
 	}
 }
