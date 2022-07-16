d23a7909f9c67c56594e685d74688ca5eccc3025
Róbert Papp <papp.robert.s@gmail.com>
2022-07-16 21:59:43 +0200

    Remove unused import

--- gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/commonJavaConfig.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/commonJavaConfig.kt
@@ -6,7 +6,6 @@ import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 import net.twisterrob.sun.plugins.isCI
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
-import org.gradle.api.artifacts.DependencyResolveDetails
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.getByName
