0b668b384ea6c843c6b7f48e909cecb5559196bc
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-07-24 22:16:50 +0100

    Fix android {}

--- gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/android.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/android.kt
@@ -1,9 +1,12 @@
 package net.twisterrob.sun.plugins.internal
 
 import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.BaseExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
 fun Project.android(block: CommonExtension<*, *, *, *>.() -> Unit) {
-	project.extensions.configure(block)
+	project.extensions.configure<BaseExtension> {
+		block(this as CommonExtension<*, *, *, *>)
+	}
 }
