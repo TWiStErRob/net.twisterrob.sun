6a23420bba7ab9ccd2de5286965244dbc08cfc94
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-07-24 22:51:46 +0100

    Reduce "project" references

--- gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/DetektReportMergePlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/DetektReportMergePlugin.kt
@@ -10,47 +10,47 @@ import org.gradle.kotlin.dsl.withType
 internal class DetektReportMergePlugin : Plugin<Project> {
 
 	override fun apply(project: Project) {
-		configureSarif(project)
-		configureXML(project)
+		project.configureSarif()
+		project.configureXML()
 	}
+}
 
-	private fun configureSarif(project: Project) {
-		project.rootProject.tasks.maybeRegister<ReportMergeTask>("detektReportMergeSarif") {
-			output.set(project.rootProject.buildDir.resolve("reports/detekt/merge.sarif"))
-		}
-		project.tasks.withType<Detekt>().configureEach {
-			reports {
-				xml.required.set(true) // Github Code Scanning
-				// https://sarifweb.azurewebsites.net
-				sarif.required.set(true) // Github Code Scanning
-			}
-		}
-		val detektReportMergeSarif =
-			project.rootProject.tasks.named<ReportMergeTask>("detektReportMergeSarif")
-		project.tasks.withType<Detekt> {
-			detektReportMergeSarif.configure {
-				mustRunAfter(this@withType)
-				input.from(this@withType.sarifReportFile)
-			}
+private fun Project.configureSarif() {
+	rootProject.tasks.maybeRegister<ReportMergeTask>("detektReportMergeSarif") {
+		output.set(rootProject.buildDir.resolve("reports/detekt/merge.sarif"))
+	}
+	tasks.withType<Detekt>().configureEach {
+		reports {
+			xml.required.set(true) // Github Code Scanning
+			// https://sarifweb.azurewebsites.net
+			sarif.required.set(true) // Github Code Scanning
 		}
 	}
+	val detektReportMergeSarif =
+		rootProject.tasks.named<ReportMergeTask>("detektReportMergeSarif")
+	tasks.withType<Detekt> {
+		detektReportMergeSarif.configure {
+			mustRunAfter(this@withType)
+			input.from(this@withType.sarifReportFile)
+		}
+	}
+}
 
-	private fun configureXML(project: Project) {
-		project.tasks.withType<Detekt>().configureEach {
-			reports {
-				xml.required.set(true) // Github Code Scanning
-			}
+private fun Project.configureXML() {
+	tasks.withType<Detekt>().configureEach {
+		reports {
+			xml.required.set(true) // Github Code Scanning
 		}
-		project.rootProject.tasks.maybeRegister<ReportMergeTask>("detektReportMergeXml") {
-			output.set(project.rootProject.buildDir.resolve("reports/detekt/merge.xml"))
-		}
-		val detektReportMergeXml =
-			project.rootProject.tasks.named<ReportMergeTask>("detektReportMergeXml")
-		project.tasks.withType<Detekt> {
-			detektReportMergeXml.configure {
-				mustRunAfter(this@withType)
-				input.from(this@withType.xmlReportFile)
-			}
+	}
+	rootProject.tasks.maybeRegister<ReportMergeTask>("detektReportMergeXml") {
+		output.set(rootProject.buildDir.resolve("reports/detekt/merge.xml"))
+	}
+	val detektReportMergeXml =
+		rootProject.tasks.named<ReportMergeTask>("detektReportMergeXml")
+	tasks.withType<Detekt> {
+		detektReportMergeXml.configure {
+			mustRunAfter(this@withType)
+			input.from(this@withType.xmlReportFile)
 		}
 	}
 }
