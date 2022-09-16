7a2d5b898b6d0dc70870023db16bbf1566f5ff2b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-16 12:05:53 +0100

    Restore

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,3 +18,29 @@ tasks.register("check") {
 	description = "Delegate task for checking included builds too."
 	dependsOn(gradle.includedBuild("plugins").task(":check"))
 }
+
+val gradleVersion: String = GradleVersion.current().baseVersion.version
+
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"Resolution of the configuration :component:core:provided was attempted from a context different than the project context. " +
+		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
+		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
+		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
+)
+
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"Resolution of the configuration :component:lib:provided was attempted from a context different than the project context. " +
+		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
+		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
+		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
+)
+
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"Resolution of the configuration :component:paparazzi:provided was attempted from a context different than the project context. " +
+		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
+		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
+		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
+)
