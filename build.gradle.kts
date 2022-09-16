113f1c917a53fe271098a2ea1409c367afa35d3f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-16 11:09:14 +0100

    Remove warning suppression

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,3 @@ tasks.register("check") {
 	description = "Delegate task for checking included builds too."
 	dependsOn(gradle.includedBuild("plugins").task(":check"))
 }
-
-// Ignore warning for https://issuetracker.google.com/issues/218478028 since Gradle 7.5,
-// it's going to be fixed in AGP 7.3.
-val gradleVersion: String = GradleVersion.current().baseVersion.version
-doNotNagAbout(
-	"IncrementalTaskInputs has been deprecated. "
-		+ "This is scheduled to be removed in Gradle 8.0. "
-		+ "On method 'IncrementalTask.taskAction\$gradle_core' use 'org.gradle.work.InputChanges' instead. "
-		+ "Consult the upgrading guide for further information: "
-		+ "https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_7.html#incremental_task_inputs_deprecation"
-)
