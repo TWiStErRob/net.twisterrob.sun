519210773516cf2e17c988ff95afdf331e976721
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-29 22:10:45 +0100

    Use doNotNagAbout from plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
-//import net.twisterrob.gradle.doNotNagAbout
+import net.twisterrob.gradle.doNotNagAbout
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODEL Gradle 7.x
 plugins {
 	id("project-module-root")
@@ -20,32 +21,32 @@ tasks.register("check") {
 	dependsOn(gradle.includedBuild("plugins").task(":check"))
 }
 
-//val gradleVersion: String = GradleVersion.current().baseVersion.version
-//
-//// region Gradle Sync warnings
-//// Currently with Gradle 7.5.1 only these 3 warnings show up.
-//// As of 2022 September Gradle 8 dev build still works with Android Studio 2021.3.1 (Dolphin).
-//// Tracking issue: https://youtrack.jetbrains.com/issue/IDEA-284158
-//// (parent https://youtrack.jetbrains.com/issue/IDEA-301941)
-//@Suppress("MaxLineLength")
-//doNotNagAbout(
-//	"Resolution of the configuration :component:core:provided was attempted from a context different than the project context. " +
-//		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
-//		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
-//		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
-//)
-//@Suppress("MaxLineLength")
-//doNotNagAbout(
-//	"Resolution of the configuration :component:lib:provided was attempted from a context different than the project context. " +
-//		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
-//		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
-//		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
-//)
-//@Suppress("MaxLineLength")
-//doNotNagAbout(
-//	"Resolution of the configuration :component:paparazzi:provided was attempted from a context different than the project context. " +
-//		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
-//		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
-//		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
-//)
-//// endregion
+val gradleVersion: String = GradleVersion.current().baseVersion.version
+
+// region Gradle Sync warnings
+// Currently with Gradle 7.5.1 only these 3 warnings show up.
+// As of 2022 September Gradle 8 dev build still works with Android Studio 2021.3.1 (Dolphin).
+// Tracking issue: https://youtrack.jetbrains.com/issue/IDEA-284158
+// (parent https://youtrack.jetbrains.com/issue/IDEA-301941)
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"Resolution of the configuration :component:core:provided was attempted from a context different than the project context. " +
+		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
+		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
+		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
+)
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"Resolution of the configuration :component:lib:provided was attempted from a context different than the project context. " +
+		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
+		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
+		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
+)
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"Resolution of the configuration :component:paparazzi:provided was attempted from a context different than the project context. " +
+		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
+		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
+		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
+)
+// endregion
