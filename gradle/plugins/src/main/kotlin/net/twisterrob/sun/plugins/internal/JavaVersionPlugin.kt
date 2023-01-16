4ccbb2135d7b1afbb160d108c2c7c44a988c3035
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-16 11:52:08 +0000

    Configure more Java versions for android

--- gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/JavaVersionPlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/JavaVersionPlugin.kt
@@ -13,6 +13,20 @@ internal class JavaVersionPlugin : Plugin<Project> {
 
 	override fun apply(project: Project) {
 		val javaVersion = project.libs.versions.javaVersion
+		project.androidOptional {
+			// TODEL Explicitly specify Java version because https://issuetracker.google.com/issues/260059413
+			// > > Task :app:compileDebugKotlin
+			// > 'compileDebugJavaWithJavac' task (current target is 1.8) and
+			// > 'compileDebugKotlin' task (current target is 11)
+			// > jvm target compatibility should be set to the same Java version.
+			// > By default will become an error since Gradle 8.0+!
+			// > Read more: https://kotl.in/gradle/jvm/target-validation
+			// > Consider using JVM toolchain: https://kotl.in/gradle/jvm/toolchain
+			compileOptions {
+				sourceCompatibility = javaVersion
+				targetCompatibility = javaVersion
+			}
+		}
 		project.tasks.withType<JavaCompile>().configureEach {
 			sourceCompatibility = javaVersion.toString()
 			targetCompatibility = javaVersion.toString()
