d8d791d01ca83f73319639efb4dcfd7322d506f5
Róbert Papp <papp.robert.s@gmail.com>
2023-01-18 22:57:43 +0000

    Update paparazzi from 1.1.0-sdk33-alpha04 to 1.2.0 stable (#193)

--- component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/PaparazziFactory.kt
+++ component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/PaparazziFactory.kt
@@ -4,13 +4,15 @@ package net.twisterrob.sun.test.screenshots
 
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams.RenderingMode
 
 fun widgetPaparazzi(): Paparazzi =
 	Paparazzi(
 		theme = "AppTheme.ScreenshotTest",
 		deviceConfig = DeviceConfig.PIXEL_2.copy(softButtons = false),
 		maxPercentDifference = 0.0,
-		appCompatEnabled = false
+		appCompatEnabled = false,
+		renderingMode = RenderingMode.SHRINK,
 	)
 
 fun activityPaparazzi(): Paparazzi =
