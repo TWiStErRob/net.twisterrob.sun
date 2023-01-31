54062191b56ebf498392e3c2e311eb660c252160
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-31 14:35:28 +0000

    Revise warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import net.twisterrob.gradle.doNotNagAbout
 
-@Suppress("DSL_SCOPE_VIOLATION") // TODEL Gradle 7.x
+@Suppress("DSL_SCOPE_VIOLATION") // TODEL https://github.com/gradle/gradle/issues/22797
 plugins {
 	id("project-module-root")
 	alias(libs.plugins.android.app) apply false
@@ -23,40 +23,35 @@ tasks.register("check") {
 
 val gradleVersion: String = GradleVersion.current().version
 
-// region Gradle Sync warnings
-// Currently with Gradle 7.5.1 only these 3 warnings show up.
-// As of 2022 September Gradle 8 dev build still works with Android Studio 2021.3.1 (Dolphin).
-// Tracking issue: https://youtrack.jetbrains.com/issue/IDEA-284158
-// (parent https://youtrack.jetbrains.com/issue/IDEA-301941)
-@Suppress("MaxLineLength")
-doNotNagAbout(
-	"Resolution of the configuration :component:core:provided was attempted from a context different than the project context. " +
-		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
-		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
-		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
-)
-@Suppress("MaxLineLength")
-doNotNagAbout(
-	"Resolution of the configuration :component:lib:provided was attempted from a context different than the project context. " +
-		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
-		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
-		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
-)
-@Suppress("MaxLineLength")
-doNotNagAbout(
-	"Resolution of the configuration :component:paparazzi:provided was attempted from a context different than the project context. " +
-		"Have a look at the documentation to understand why this is a problem and how it can be resolved. " +
-		"This behaviour has been deprecated and is scheduled to be removed in Gradle 8.0. " +
-		"See https://docs.gradle.org/${gradleVersion}/userguide/viewing_debugging_dependencies.html#sub:resolving-unsafe-configuration-resolution-errors for more details."
-)
-// Android Studio 2022.1.1 (Electric Eel)
+// TODEL Gradle sync in AS EE 2022.1.1 https://youtrack.jetbrains.com/issue/IDEA-301430, fixed in AS Giraffe.
 @Suppress("MaxLineLength")
 doNotNagAbout(
 	"The org.gradle.util.GUtil type has been deprecated. " +
 		"This is scheduled to be removed in Gradle 9.0. " +
-		"Consult the upgrading guide for further information: https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_7.html#org_gradle_util_reports_deprecations"
+		"Consult the upgrading guide for further information: " +
+		"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_7.html#org_gradle_util_reports_deprecations",
+	"at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl\$_getSourceSets_closure"
 )
-// endregion
+
+// TODEL Gradle sync in AS EE 2022.1.1 https://youtrack.jetbrains.com/issue/IDEA-306975, maybe fixed in AS H.
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+		"This is scheduled to be removed in Gradle 9.0. " +
+		"Please use the archiveFile property instead. " +
+		"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
+	"at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl\$_getSourceSets_closure"
+)
+
+// TODEL Gradle sync in AS EE 2022.1.1 https://youtrack.jetbrains.com/issue/IDEA-306975, maybe fixed in AS H.
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+		"This is scheduled to be removed in Gradle 9.0. " +
+		"Please use the archiveFile property instead. " +
+		"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
+	"at org.jetbrains.plugins.gradle.tooling.util.SourceSetCachedFinder.createArtifactsMap"
+)
 
 // TODEL https://issuetracker.google.com/issues/264177800
 if (com.android.Version.ANDROID_GRADLE_PLUGIN_VERSION < "7.4.1") {
