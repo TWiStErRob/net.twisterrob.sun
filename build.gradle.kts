001ef344addff814f0ba7c74c970f89168f42689
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-14 11:10:31 +0100

    Remove hack, fixed in AGP 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,3 @@ tasks.register("check") {
 	description = "Delegate task for checking included builds too."
 	dependsOn(gradle.includedBuild("plugins").task(":check"))
 }
-
-// TODEL https://issuetracker.google.com/issues/247906487
-if (com.android.Version.ANDROID_GRADLE_PLUGIN_VERSION.startsWith("7.")) {
-	val loggerFactory: org.slf4j.ILoggerFactory = org.slf4j.LoggerFactory.getILoggerFactory()
-	val addNoOpLogger: java.lang.reflect.Method = loggerFactory.javaClass
-		.getDeclaredMethod("addNoOpLogger", String::class.java)
-		.apply {
-			isAccessible = true
-		}
-	addNoOpLogger(loggerFactory, "com.android.build.api.component.impl.MutableListBackedUpWithListProperty")
-	addNoOpLogger(loggerFactory, "com.android.build.api.component.impl.MutableMapBackedUpWithMapProperty")
-} else {
-	error("AGP major version changed, review hack.")
-}
