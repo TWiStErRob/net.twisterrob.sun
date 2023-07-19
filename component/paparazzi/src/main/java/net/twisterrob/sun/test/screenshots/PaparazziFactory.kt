1701faee4d3a58bf4c1676b02a63d2911dc18ad5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-19 14:53:12 +0100

    Re-think parameters

--- component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/PaparazziFactory.kt
+++ component/paparazzi/src/main/java/net/twisterrob/sun/test/screenshots/PaparazziFactory.kt
@@ -6,19 +6,26 @@ import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import com.android.ide.common.rendering.api.SessionParams.RenderingMode
 
+/**
+ * Set up Paparazzi for widgets, components, and other small UI elements.
+ */
 fun widgetPaparazzi(): Paparazzi =
 	Paparazzi(
 		theme = "AppTheme.ScreenshotTest",
-		deviceConfig = DeviceConfig.PIXEL_2.copy(softButtons = false),
 		maxPercentDifference = 0.0,
 		appCompatEnabled = false,
+		showSystemUi = false,
 		renderingMode = RenderingMode.SHRINK,
 	)
 
+/**
+ * Set up Paparazzi for Activity tests, which take the full screen (not fullscreen).
+ */
 fun activityPaparazzi(): Paparazzi =
 	Paparazzi(
 		theme = "AppTheme",
 		deviceConfig = DeviceConfig.PIXEL_2,
 		maxPercentDifference = 0.0,
-		appCompatEnabled = true
+		appCompatEnabled = true,
+		showSystemUi = true,
 	)
