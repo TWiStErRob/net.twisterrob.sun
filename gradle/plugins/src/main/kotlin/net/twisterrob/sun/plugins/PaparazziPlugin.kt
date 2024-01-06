2c5ab010a2733d6b84db6fe983b5a93e5fb5d7a4
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-01-06 17:20:51 +0000

    Remove unused imports

--- gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/PaparazziPlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/PaparazziPlugin.kt
@@ -2,10 +2,8 @@ package net.twisterrob.sun.plugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.withType
 
 public class PaparazziPlugin : Plugin<Project> {
