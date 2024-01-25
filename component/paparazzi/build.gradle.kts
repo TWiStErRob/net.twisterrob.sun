4870a2879bbc9092b65dac43aee8466b83548653
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-01-25 09:15:10 +0000

    Move

--- component/paparazzi/build.gradle.kts
+++ component/paparazzi/build.gradle.kts
@@ -7,21 +7,6 @@ dependencies {
 	api(libs.test.junit4)
 	api(libs.test.mockito)
 	api(libs.test.paramInjector)
-
-	constraints {
-		api("com.google.guava:guava") {
-			attributes {
-				attribute(
-					TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
-					objects.named<TargetJvmEnvironment>(TargetJvmEnvironment.STANDARD_JVM)
-				)
-			}
-			because(
-				"LayoutLib and sdk-common depend on Guava's -jre published variant." +
-					"See https://github.com/cashapp/paparazzi/issues/906 ."
-			)
-		}
-	}
 }
 
 // See https://github.com/cashapp/paparazzi/issues/1025#issuecomment-1687437843.
