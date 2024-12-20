840fd2d346505d0eda73c91f4e05bc1ba53e8212
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-12-20 00:56:30 +0000

    Suppress new problems

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
-import net.twisterrob.gradle.settings.enableFeaturePreviewQuietly
 import net.twisterrob.gradle.doNotNagAbout
+import net.twisterrob.gradle.settings.enableFeaturePreviewQuietly
 import net.twisterrob.sun.plugins.isCI
 import java.util.UUID
 
@@ -95,3 +95,32 @@ develocity {
 }
 
 val gradleVersion: String = GradleVersion.current().version
+
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	Regex(
+		"""The (.+?)RuntimeClasspathCopy configuration has been deprecated for consumption\. """ +
+				Regex.escape(
+					"This will fail with an error in Gradle 9.0. " +
+							"For more information, please refer to " +
+							"https://docs.gradle.org/${gradleVersion}/userguide/declaring_dependencies.html#sec:deprecated-configurations" +
+							" in the Gradle documentation."
+				) +
+				".*"
+	),
+)
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	Regex(
+		"""While resolving configuration '(.+?)RuntimeClasspathCopy', it was also selected as a variant\. """ +
+				Regex.escape(
+					"Configurations should not act as both a resolution root and a variant simultaneously. " +
+							"Depending on the resolved configuration in this manner has been deprecated. " +
+							"This will fail with an error in Gradle 9.0. " +
+							"Be sure to mark configurations meant for resolution as canBeConsumed=false or use the 'resolvable(String)' configuration factory method to create them. " +
+							"Consult the upgrading guide for further information: " +
+							"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#depending_on_root_configuration"
+				) +
+				".*"
+	),
+)
