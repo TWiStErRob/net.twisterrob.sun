69327c0dc0316e46d236625a1c1ab09cd1d1e76f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-02-01 14:53:29 +0000

    Reduce noise when running tests

--- gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/AndroidPlugin.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/sun/plugins/internal/AndroidPlugin.kt
@@ -1,7 +1,9 @@
 package net.twisterrob.sun.plugins.internal
 
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 
 internal class AndroidPlugin : Plugin<Project> {
 
@@ -18,6 +20,16 @@ internal class AndroidPlugin : Plugin<Project> {
 					.file("config/lint/baseline/lint_baseline-${project.slug}.xml")
 				lintConfig = project.rootProject
 					.file("config/lint/lint.xml")
+			}
+			testOptions {
+				unitTests {
+					all { task: Test ->
+						// Hide (https://stackoverflow.com/a/79098701/253468)
+						// > Java HotSpot(TM) 64-Bit Server VM warning:
+						// > Sharing is only supported for boot loader classes because bootstrap classpath has been appended
+						task.jvmArgs("-Xshare:off")
+					}
+				}
 			}
 		}
 	}
